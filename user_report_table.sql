caf1ec2e0ee6ba239880d85c836da650498fc002
Simina Mannan Trisha <63448204+Siminatrisha@users.noreply.github.com>
2024-08-26 21:04:11 +0600

    Add files via upload

---
+++ user_report_table.sql
@@ -0,0 +1,23 @@
+insert into user_reports_table (Name, Email,Phone_no, Address, Registration_no, Vehicle_type,Manufacturer, Model,Fuel_type,Year,Insurer,Premium,IDV,Amount)
+values( 'Simina mannan trisha', 'siminatrisha1@gmail.com','01620526885','Dhaka','DKA_334','car','Honda','Honda WRV','Petrol','21 Jan 2018','Pragati Insurance Limited',1231, 5762,38832);
+insert into user_reports_table (Name, Email,Phone_no, Address, Registration_no, Vehicle_type,Manufacturer, Model,Fuel_type,Year,Insurer,Premium,IDV,Amount)
+values( 'Adib Rahman', 'adibrahman1@gmail.com','01355676309','Chittagong','CTG-2314','car','AUDI','A3','Petrol','10 Jun 2019','Janata Insurance Limited',4523, 999,3832);
+
+DELETE FROM user_reports_table WHERE Email='adibrahman1@gmail.com';
+DELETE FROM user_reports_table WHERE Email='siminatrisha1@gmail.com';
+DELETE FROM user_reports_table WHERE Email='siminatrisha353@gmail.com';
+
+insert into user_reports_table (Name, Email,Phone_no, Address, Registration_no, Vehicle_type,Manufacturer, Model,Fuel_type,Year,Insurer,Premium,IDV,Amount)
+values( 'Kamal Hasan', 'hasankamal@gmail.com','01674962121','Rajshahi','Jes_5621','car','Mahindra','Mahindra Verrito','Disel','09 Mar 2015','Crystal Insurance Limited',1901, 5462,30972);
+insert into user_reports_table (Name, Email,Phone_no, Address, Registration_no, Vehicle_type,Manufacturer, Model,Fuel_type,Year,Insurer,Premium,IDV,Amount)
+values( 'MD Abdul Rahman', 'abdulrahman1@gmail.com','01355887540','Khulna','DKA-2567','car','Maruti Suzuki','Maruti Suzuki Ciaz','Petrol','17 Apr 2019','Janata Insurance Limited',4336, 9120,38392);
+
+insert into user_reports_table (Name, Email,Phone_no, Address, Registration_no, Vehicle_type,Manufacturer, Model,Fuel_type,Year,Insurer,Premium,IDV,Amount)
+values( 'Nomanur Rahman', 'nomanurrahman@gmail.com','01624323841','Dhaka','Metro-ta-223','Bike','Yamaha','Yamaha R15 v4','Petrol','10 Jan 2018','Pragati Insurance Limited',1091, 32662,30965);
+insert into user_reports_table (Name, Email,Phone_no, Address, Registration_no, Vehicle_type,Manufacturer, Model,Fuel_type,Year,Insurer,Premium,IDV,Amount)
+values( 'Atik Islam', 'islamatik@gmail.com','01355676309','Jessor','Jes-2624','car','BMW','BMW X1','Petrol','22 Mar 2019','Reliance Insurance Limited',4323, 1299,38312);
+
+insert into user_reports_table (Name, Email,Phone_no, Address, Registration_no, Vehicle_type,Manufacturer, Model,Fuel_type,Year,Insurer,Premium,IDV,Amount)
+values( 'Arif Hossain', 'arif hossain@gmail.com','01327828134','Barishal','Metro-gha-432','Bike','Hero Motorcorp','Hero XPULSE 200','Diesel','31 Mar 2015','Crystal Insurance Limited',1000, 8792,30965);
+insert into user_reports_table (Name, Email,Phone_no, Address, Registration_no, Vehicle_type,Manufacturer, Model,Fuel_type,Year,Insurer,Premium,IDV,Amount)
+values( 'Rehana Rahman', 'rehanarahman@gmail.com','01359227612','Kushtia','Jes-783','car','BMW','BMW 5 Series','Petrol',' 07 Sep 2019','Reliance Insurance Limited',4323, 1299,38312);
